3fcd9a478ee05465b824afe2ae1e1368e30874e5
Konstantin-S06 <konstantin.shtop@gmail.com>
2025-09-29 15:37:42 -0400

    Update NumberTriangle.java
    Updated the code

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -90,17 +90,18 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
+
         if (path.isEmpty()) {
             return this.root;
         }
-        else if (path.substring(0, 1).equals("l")) {
+        else if (path.charAt(0) == ('l')){
             return this.left.retrieve(path.substring(1));
         }
-        else {
+        else
+        {
             return this.right.retrieve(path.substring(1));
         }
     }
-
 
     /** Read in the NumberTriangle structure from a file.
      *
